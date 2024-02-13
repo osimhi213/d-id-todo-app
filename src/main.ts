3fd0f90a36f3ead554db15a1bdfaa9201830526e
Ofek Simhi <osimhi@Ofeks-MacBook-Pro.local>
2024-02-13 21:05:34 +0200

    todoApp TS with fixes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 
 interface Task {
   label: string;
-  status: "done" | "undone";
+  status?: "done" | "undone";
 }
 
 const taskList = getHTMLElementById("task-list");
@@ -13,19 +13,23 @@ const searchBoxDeleteBtn = getHTMLElementById("search-delete-button");
 const newTaskInput = getHTMLElementById("new-task-text") as HTMLInputElement;
 const taskAddBtn = getHTMLElementById("new-task-add-button");
 
-function isNull(val: any): val is null {
-  return val === null;
-}
-
-function isValidStatus(status: string | null): status is Task["status"] {
-  return !isNull(status) && (status === "done" || status === "undone");
+function isValidStatus(
+  status: string | null | undefined
+): status is Task["status"] {
+  return (
+    status !== null &&
+    status !== undefined &&
+    (status === "done" || status === "undone")
+  );
 }
 
 function getHTMLElementById(id: string): HTMLElement {
   const htmlElement = document.getElementById(id);
-  if (!isNull(htmlElement)) {
+
+  if (htmlElement !== null) {
     return htmlElement;
   }
+
   throw new Error("No matching element was found in the document!");
 }
 
@@ -35,56 +39,56 @@ function getNewTaskElement(task: Task): HTMLLIElement {
   const statusBtnElement = document.createElement("button");
   const trashElement = document.createElement("i");
 
-  newTaskElement.setAttribute("status", task.status);
-
   newTaskElement.classList.add("task");
-  statusBtnElement.classList.add("status");
   trashElement.classList.add("gg-trash");
+  statusBtnElement.classList.add("status");
 
   labelElement.textContent = task.label;
   statusBtnElement.textContent = "Done";
+
+  newTaskElement.setAttribute("status", task.status || "undone");
 
   newTaskElement.appendChild(trashElement);
   newTaskElement.appendChild(labelElement);
   newTaskElement.appendChild(statusBtnElement);
 
+  statusBtnElement.addEventListener("click", () => {
+    changeTaskStatus(newTaskElement);
+  });
+  trashElement.addEventListener("click", () => {
+    deleteTask(newTaskElement);
+  });
+
   return newTaskElement;
 }
 
-function addNewTask(
-  label: Task["label"],
-  status: Task["status"] = "undone"
-): void {
-  if (label.trim()) {
-    const task: Task = { label, status };
-    const newTaskElement = getNewTaskElement(task);
-    taskList.appendChild(newTaskElement);
+function addNewTask(task: Task): void {
+  if (!task.label.trim()) {
+    console.log("Cannot add an empty task");
+  } else {
+    taskList.appendChild(getNewTaskElement(task));
     storeTaskList();
     displaySearchedList();
-  } else {
-    console.log("Cannot add an empty task");
   }
 }
 
 function changeTaskStatus(taskElement: HTMLElement): void {
-  if (!isNull(taskElement)) {
-    const statusAttribure = taskElement.getAttribute("status");
-    if (isValidStatus(statusAttribure)) {
-      const taskStatus = statusAttribure;
-      taskElement.setAttribute(
-        "status",
-        taskStatus === "done" ? "undone" : "done"
-      );
-    } else {
-      throw new Error(
-        "Got incompatible task status. should be of type Task['status']"
-      );
-    }
-  } else {
+  if (taskElement === null) {
     throw new Error(
       "The status button either has no parent, or its parent isn't a DOM Element"
     );
   }
+
+  const taskStatus = taskElement.getAttribute("status");
+
+  if (!isValidStatus(taskStatus)) {
+    throw new Error(
+      "Got incompatible task status. should be of 'done' or 'undone'"
+    );
+  }
+
+  taskElement.setAttribute("status", taskStatus === "done" ? "undone" : "done");
+
   storeTaskList();
 }
 
@@ -96,25 +100,28 @@ function deleteTask(taskElement: HTMLElement): void {
 function flushSearchBox(): void {
   searchTextInput.value = "";
 
-  for (let i = 0; i < taskList.children.length; i++) {
-    const taskElement = taskList.children[i];
+  for (const taskElement of taskList.children) {
     if (taskElement instanceof HTMLElement) taskElement.style.display = "flex";
   }
 }
 
 function getTaskLabel(taskElement: HTMLElement): Task["label"] {
   const taskLabel = taskElement.querySelector("label")?.textContent;
-  if (taskLabel) return taskLabel;
-  throw new Error("Got no task or textContent");
+
+  if (!taskLabel) {
+    throw new Error("Got no task or textContent");
+  }
+
+  return taskLabel;
 }
 
 function displaySearchedList(): void {
   const searchPattern = searchTextInput.value.toLowerCase();
 
-  for (let i = 0; i < taskList.children.length; i++) {
-    const taskElement = taskList.children[i];
+  for (const taskElement of taskList.children) {
     if (taskElement instanceof HTMLElement) {
       const taskLabel = getTaskLabel(taskElement);
+
       taskElement.style.display = taskLabel
         .toLowerCase()
         .includes(searchPattern)
@@ -125,23 +132,23 @@ function displaySearchedList(): void {
 }
 
 function taskList2JsonString(): string {
-  const taskList2Save: [Task["label"], Task["status"]][] = [];
+  const taskList2Save: Task[] = [];
 
-  for (let i = 0; i < taskList.children.length; i++) {
-    const taskElement = taskList.children[i];
+  for (const taskElement of taskList.children) {
     if (taskElement instanceof HTMLElement) {
       const taskLabel = getTaskLabel(taskElement);
-      const statusAttribure = taskElement.getAttribute("status");
-      if (isValidStatus(statusAttribure)) {
-        const taskStatus = statusAttribure;
-        taskList2Save.push([taskLabel, taskStatus]);
-      } else {
+      const taskStatus = taskElement.getAttribute("status");
+
+      if (!isValidStatus(taskStatus)) {
         throw new Error(
-          "Got incompatible task status. should be of type Task['status']"
+          "Got incompatible task status. Should be of done or undone"
         );
       }
+
+      taskList2Save.push({ label: taskLabel, status: taskStatus });
     }
   }
+
   return JSON.stringify(taskList2Save);
 }
 
@@ -151,27 +158,12 @@ function storeTaskList(): void {
 
 function retrieveTaskList(): void {
   const storedTaskList = localStorage.getItem("storedTaskList");
-  if (!isNull(storedTaskList)) {
-    const parsedTaskList: [Task["label"], Task["status"]][] =
-      JSON.parse(storedTaskList);
-    for (const idx in parsedTaskList) {
-      const [label, status] = parsedTaskList[idx];
-      addNewTask(label, status);
-    }
-  }
-}
 
-function handleBtnClick(event: Event) {
-  const { target } = event;
-  const ClickedBtn = target as HTMLButtonElement;
-  if (ClickedBtn) {
-    const parentElement = ClickedBtn.parentElement;
-    if (!isNull(parentElement)) {
-      if (ClickedBtn.matches("li.task > button.status")) {
-        changeTaskStatus(parentElement);
-      } else if (ClickedBtn.matches("li.task > .gg-trash")) {
-        deleteTask(parentElement);
-      }
+  if (storedTaskList !== null) {
+    const parsedTaskList: Task[] = JSON.parse(storedTaskList);
+
+    for (const taskElement of parsedTaskList) {
+      addNewTask(taskElement);
     }
   }
 }
@@ -179,20 +171,19 @@ function handleBtnClick(event: Event) {
 (function myApp() {
   taskList.innerHTML = "";
   retrieveTaskList();
+
   taskAddBtn.addEventListener("click", () => {
-    addNewTask(newTaskInput.value);
+    addNewTask({ label: newTaskInput.value });
     newTaskInput.value = "";
   });
 
   newTaskInput.addEventListener("keypress", (event) => {
     if (event.code === "Enter") {
-      addNewTask(newTaskInput.value);
+      addNewTask({ label: newTaskInput.value });
       newTaskInput.value = "";
     }
   });
 
   searchBoxDeleteBtn.addEventListener("click", flushSearchBox);
   searchTextInput.addEventListener("input", displaySearchedList);
-  document.body.addEventListener("click", handleBtnClick);
-
 })();
